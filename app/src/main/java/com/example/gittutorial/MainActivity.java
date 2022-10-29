fc8f34dc8f9af6def31143169a467eccafafb85d
Rrucc <rrucc@>
2022-10-29 13:45:08 +0200

    include print

--- app/src/main/java/com/example/gittutorial/MainActivity.java
+++ app/src/main/java/com/example/gittutorial/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
+        System.out.println("Ejemplo");
     }
 }
